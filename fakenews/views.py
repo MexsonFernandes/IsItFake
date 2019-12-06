b2136bec064b43110ac20a56fbdd171108037d12
Mexson Fernandes <robomex2020@gmail.com>
2019-12-06 18:57:53 +0530

    added fake news

--- fakenews/views.py
+++ fakenews/views.py
@@ -1,5 +1,77 @@
+import numpy as np
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from newspaper import Article
+import pickle
+from django.conf import settings
+import traceback
 
 
 def home(request):
     return render(request, 'fakenews/index.html')
+
+
+def headline_text(url = '', summary = False):
+    '''
+    Returns headline text of news..
+    Just makes call to the newspaper class.
+    '''
+    article = Article(url)
+    # Download the article and parse it.
+    article.download()
+    article.parse()
+    #authors = article.authors
+    headline = article.title
+    text = article.text
+    if summary:
+        article.nlp()
+        summary = article.summary
+        return url, headline, text, summary
+    return url, headline, text
+
+
+model = pickle.load(open(settings.MEDIA_URL + 'fakenews/model.sav', 'rb'))
+vec = pickle.load(open(settings.MEDIA_URL + 'fakenews/vec_fnd.pkl', 'rb'))
+
+
+def predict(text):
+    pred = vec.transform(np.array([text]))
+    out = model.predict(pred)
+    return out[0]
+
+
+@csrf_exempt
+def home(request):
+    context = {
+    }
+    if request.POST:
+        try:
+            print(request.POST)
+            out = 0
+            context = {
+                'msg': 'output',
+                'input': '',
+                'out': out
+            }
+            if 'text-input' in request.POST:
+                text = request.POST.get('text-input', '')
+                out = predict(text)
+                context['out'] = 'fake' if out == 1 else 'real'
+                context['input'] = text
+            else:
+                url = request.POST.get('url')
+                _, head, content = headline_text(url)
+                print(content)
+                out = predict(content)
+                context['out'] = 'fake' if out == 1 else 'real'
+                context['input'] = content
+
+        except Exception as e:
+            print(str(e))
+            print(traceback.print_exc())
+            if 'text-input' in request.POST:
+                context['error'] = 'Error in parsing data'
+            else:
+                context['error'] = 'Link is incorrect'
+    return render(request, 'fakenews/index.html', context)
+
