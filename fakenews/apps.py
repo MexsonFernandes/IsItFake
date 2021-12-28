3759f58117a9f4bb37f94b3f531d6bd4e52d93cb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-28 17:43:36 +0800

    Delete fakenews directory

--- fakenews/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FakenewsConfig(AppConfig):
-    name = 'fakenews'
--- fakenews/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-
-class UserInputModel(models.Model):
-    added_on = models.DateTimeField(default=timezone.now)
-    news = models.CharField(max_length=5000)
-    url = models.URLField()
-    output = models.CharField(max_length=50)
